a1e7f0e1419d9811e30c10e7e5928ff5f8c8896d
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-03-04 00:08:40 +0500

    Update App.js

--- existingHr/src/App.js
+++ existingHr/src/App.js
@@ -1,22 +1,23 @@
-// import './App.css'
-import React from 'react';
-import Login from "./Components/newLogin/Login"
-import Dashboard from './Components/Dashboard';
-import LeaveRequest from './Components/LeaveRequest/LeaveRequest';
-import EmployeePortal from './Components/EmployeePortal/EmployeePortal'
-import PayrollBenefit from './Components/PayrollBenefit';
+// importing React library, routes, and different components/pages of our web application
+import React from 'react'; 
+import Login from "./Components/newLogin/Login"; 
+import LeaveRequest from './Components/LeaveRequest/LeaveRequest'; 
+import EmployeePortal from './Components/EmployeePortal/EmployeePortal'; 
+import PayrollBenefit from './Components/PayrollBenefit'; 
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-/* import { useState } from 'react'; */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 
+// This is the main component of our application
 function App() {
   return (
+    // Root HTML element with className "App"
     <div className="App">
+      {/* We set up the routing for different components using BrowserRouter */}
       <Router>
+        {/* Routes component defines the routes for different URLs */}
         <Routes>
-          <Route path="/EmployeePortal" element={<EmployeePortal />} /> 
-          <Route path="/LeaveRequest" element={<LeaveRequest />} /> 
-          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/EmployeePortal" element={<EmployeePortal />} />
+          <Route path="/LeaveRequest" element={<LeaveRequest />} />
           <Route path="/PayrollBenefit" element={<PayrollBenefit />} />
           <Route path="/" element={<Login />} />
         </Routes>
@@ -25,7 +26,8 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
+
 
 
 
