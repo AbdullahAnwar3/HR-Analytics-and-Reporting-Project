d113f3b55b04b1c74eeb3ba45cbc1185653a59d5
meesum70110 <meesum70110@gmail.com>
2024-03-14 14:46:43 +0500

    Updates

---
+++ precache-manifest.861af6f8042670faf90e5797c5374fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e33812aa6c1402975bfc9990b457e6",
+    "url": "/HR-Analytics-and-Reporting-Project/index.html"
+  },
+  {
+    "revision": "f075dfcec0ad64c65f8f",
+    "url": "/HR-Analytics-and-Reporting-Project/static/css/main.2bd279a6.chunk.css"
+  },
+  {
+    "revision": "900b87366d7daf02529b",
+    "url": "/HR-Analytics-and-Reporting-Project/static/js/2.6c320300.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/HR-Analytics-and-Reporting-Project/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "f075dfcec0ad64c65f8f",
+    "url": "/HR-Analytics-and-Reporting-Project/static/js/main.3c643cc7.chunk.js"
+  },
+  {
+    "revision": "79f16227fe41b1bc7da5",
+    "url": "/HR-Analytics-and-Reporting-Project/static/js/runtime~main.2883814b.js"
+  },
+  {
+    "revision": "326e6a11c5171f09ac46aad3f2e48b52",
+    "url": "/HR-Analytics-and-Reporting-Project/static/media/Devsinc_logo.326e6a11.png"
+  }
+]);
