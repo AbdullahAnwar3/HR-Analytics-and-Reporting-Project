d735047ebce2ba3ce6752c03a7d1ec2e82fdfcf9
HassanAamir089 <144484661+HassanAamir089@users.noreply.github.com>
2024-04-18 00:11:22 +0500

    Update index.js

--- Project/server/index.js
+++ Project/server/index.js
@@ -103,8 +103,8 @@ io.on('connection', (socket) => {
 
 exports.io = io
 
-mongoose.connect(process.env.MONG_URI)
-.then(()=>{
+// mongoose.connect(process.env.MONG_URI)
+mongoose.connect('mongodb+srv://HR-Admin:J3xFNzZt2ZeZxiFY@hr-analytics.ar4bs5x.mongodb.net/').then(()=>{
     server.listen(process.env.PORT, ()=>{
     console.log(`listening on port ${process.env.PORT}`);
     console.log("Connected to Database");
